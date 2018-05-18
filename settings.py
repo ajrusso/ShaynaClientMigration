cb42a00a8415ee27594bdf6c98c43f00873a1481
ajrusso <born.smasher@gmail.com>
2018-05-17 17:31:31 -0700

    Update settings.py
    Added Vaccine file information

--- settings.py
+++ settings.py
@@ -1,12 +1,15 @@
 # Settings File
 #
-dir = "C:/Users/itali_000/Documents/Tech Projects/Software Design/Shayna/"
+dir = "C:/Users/itali_000/Documents/Tech Projects/Software Design/Shayna/Data_05_04_18/"
 # Pet123 Client Data Configuration
-client123_filepath = dir + 'Clients_11_13_17.xlsx'
+client123_filepath = dir + 'Clients.xlsx'
 
 # Pet123 Pet Data Configuration
-pet123_filepath = dir + 'Pets_11_13_17.xlsx'
+pet123_filepath = dir + 'Pets.xlsx'
+
+# Pet123 Vaccination Data Configuration
+vaccine123_filepath = dir + 'Vaccinations.xlsx'
 
 # Revolation Pet Client Data Configuration
-clientrev_filepath = dir + 'customers-10.xlsx'
+clientrev_filepath = dir + 'customers.xlsx'
 new_clientrev_filepath = dir + 'new_customers.xlsx'
